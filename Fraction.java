03f2a0a530316f25099bac0a394ee7d847e51156
cfoo20 <cfoo20@punahou.edu>
2020-04-28 16:10:27 +0000

    first commit

---
+++ Fraction.java
@@ -0,0 +1,17 @@
+// Fraction class
+public class Fraction
+{
+    // instance varibles for numerator and denominator
+    private int num;
+    private int den;
+
+    // Constructor
+    // two-parameter
+    public Fraction(int num, int den)
+    {
+        this.num = num;
+        this.den = den;
+    }
+    // one parameter
+    // no parameters
+}
