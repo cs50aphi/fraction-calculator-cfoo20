3c46162c40f4c982d9b2ca227c692be7c53b1eaa
cfoo20 <cfoo20@punahou.edu>
2020-04-28 21:47:04 +0000

    instances and constructors

--- Fraction.java
+++ Fraction.java
@@ -10,8 +10,24 @@ public class Fraction
     public Fraction(int num, int den)
     {
         this.num = num;
+        // Illegal Argument Exception of denominator is 0
         this.den = den;
+        // negative goes to numerator or cancels
     }
-    // one parameter
-    // no parameters
+    // one parameter (only numerator)
+    public Fraction(int num)
+    {
+        this(num, 1);
+    }
+    // no parameters (0)
+    public Fraction()
+    {
+        this(0, 1);
+    }
+
+    // Methods
+    public int getNumerator()
+    {
+
+    }
 }
