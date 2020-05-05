47b8f457cf008880c99f408fef12e678a0ce34cc
cfoo20 <cfoo20@punahou.edu>
2020-05-05 01:12:37 +0000

    intro

---
+++ FractionCalculator.java
@@ -0,0 +1,14 @@
+// Fraction Calculator by Cheyenne Foo
+import java.util.Scanner;
+
+public class FractionCalculator
+{
+    public static void main(String[] args)
+    {
+        Scanner kb = new Scanner(System.in);
+        // Print intro
+        System.out.println("Welcome to the Fraction Calculator!");
+        System.out.println("It will add, subtract, multiply, and divide fractions until you type Q to quit.\nPlease enter your fractions in the form a/b where a and b are both integers.");
+        System.out.println("-----------------------------");
+    }
+}
